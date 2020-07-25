b021dd3e4a5d4205c9f5c25d5629a6de0592809f
Denis Vlaskin <den_sam@mail.ru>
2020-07-26 00:41:59 +0400

    Initial Commit

---
+++ LearningObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearningObjC
+//
+//  Created by Denis on 26.07.2020.
+//  Copyright © 2020 Melandra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
